976a9970d4ac9e5346e0b43c140303ff0f0ed2ff
nikolaeva <st119498@student.spbu.ru>
2024-06-14 07:49:32 +0000

    added mlmodels and source code for predicting prices

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask, request
+import joblib
+import numpy
+
+MODEL_PATH ='mlmodels/model.pkl'
+SCALER_X_PATH ='mlmodels/scaler_x.pkl'
+SCALER_Y_PATH ='mlmodels/scaler_y.pkl'
 
 app = Flask(__name__)
+model = joblib.load(MODEL_PATH)
+sc_x = joblib.load(SCALER_X_PATH)
+sc_y = joblib.load(SCALER_Y_PATH)
 
 @app.route('/predict_price', methods = ['GET'])
 def predict():
@@ -10,8 +19,13 @@ def predict():
     area = args.get('area', default =-1, type=float)
     renovation = args.get('renovation', default =-1, type=float)
 
-    responce = "open_plan:{}, rooms:{}, area:{}, renovation:{}".format(open_plan, rooms, area, renovation)
+    x = numpy.array([open_plan, rooms, area, renovation]).reshape(1,-1)
+    x = sc_x.transform(x)
 
-    return responce
+    result = model.predict(x)
+    result = sc_y.inverse_transform(result.reshape(1,-1))
+
+    return str(result[0][0])
+
 if __name__ == '__main__':
     app.run(debug = True, port= 7789, host = '0.0.0')
